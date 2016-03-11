2917744658a614fcaab594edb623a85e08d18160
tiago-pereira <tiagops.prog@gmail.com>
2016-03-10 21:43:11 -0300

    Initial commit

---
+++ src/core.js
@@ -0,0 +1,46 @@
+import {List, Map} from 'immutable';
+
+export const INITIAL_STATE = Map();
+
+export function setEntries(state, entries) {
+  return state.set('entries', List(entries));
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
+}
+
+export function vote(state, entry) {
+  /*
+  What the code expresses is "reach into the nested data structure path
+  ['vote', 'tally', 'Trainspotting'], and apply this function there.
+  If there are keys missing along the path, create new Maps in their place.
+  If the value at the end is missing, initialize it with 0".
+  */
+  return state.updateIn(
+    ['tally', entry],
+    0,
+    tally => tally + 1
+  );
+}
+
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes)  return [a];
+  else if (aVotes < bVotes)  return [b];
+  else                       return [a, b];
+}
